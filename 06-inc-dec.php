4442aa14de9dca712a7a92ed2981116eef6bbd07
juanmcr06 <juanmcr.06@gmail.com>
2022-12-08 17:26:56 -0300

    update

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
 
+echo ++$numero1; // Incremento en 1, poniendo adelante los signos ++
+echo "<br>";
 
+$numero2 = 11;
+
+echo --$numero2; // Decremento en 1, poniendo adelante los signos --
+echo "<br>";
+
+$numero3 = 10;
+
+echo $numero3 += 5; // Incremento por el numero que se indica despues del =
+
+// Si se colocan los signos luego de la variable, este incrementa o decrementa
+// Luego de imprimir la variable. Al imprimirla no hay cambio, pero en la segunda
+// Vez que lo imprimimos si lo hace con el cambio ya.
 
 include 'includes/footer.php';
--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,59 @@
 <?php include 'includes/header.php';
 
+$nombreCliente = "Juan Manuel Vivas";
 
+// Conocer Extension de un String
 
+echo strlen($nombreCliente);
+echo "<br>";
+
+// Eliminar espacios en Blanco
+
+$texto = trim($nombreCliente);
+echo $texto;
+echo "<br>";
+var_dump($texto);
+echo "<br>";
+
+// Convertir Texto a matusculas
+
+echo strtoupper($nombreCliente);
+echo "<br>";
+
+// Covertir texto a minusculas
+
+echo strtolower($nombreCliente);
+echo "<br>";
+
+// Ejemplo practico de comparar minusculas y mayusculas en un mail
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump(strtolower($mail1) == strtolower($mail2));
+echo "<br>";
+
+// Reemplazar texto de un String
+
+echo str_replace('Juan Manuel', 'J. M.', $nombreCliente);
+echo "<br>";
+
+// Revisar si existe un String
+
+echo strpos($nombreCliente, 'Vivas');
+echo "<br>";
+
+// Concatenar Strings modo 1
+
+$tipoCliente = "Premium";
+
+echo "El Cliente " . $nombreCliente . " es " . $tipoCliente;
+echo "<br>";
+
+// Concatenar Strings modo 2
+// Solo Funciona con Comillas dobles "
+
+echo "El Cliente {$nombreCliente} es {$tipoCliente}";
+echo "<br>";
 
 include 'includes/footer.php';
