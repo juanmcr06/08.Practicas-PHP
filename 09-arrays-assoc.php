d74847dcc9c8df02c6ddeca9d834db6a54b1dec2
juanmcr06 <juanmcr.06@gmail.com>
2022-12-12 03:07:22 -0300

    update

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium'
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+echo "<br>";
+
+// echo $cliente['nombre'];
+// echo "<br>";
+// echo $cliente['informacion']['tipo'];
+// echo "<br>";
+
+$cliente['id'] = 11212112;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+echo "<br>";
 
 
 include 'includes/footer.php';
